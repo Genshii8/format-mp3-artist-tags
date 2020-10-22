1aeca8c31f46be41575d477c11f55f587d359193
Adam Langbert <adamhl@pm.me>
2020-10-22 22:23:24 +0200

    remove --noFeat option. code cleanup/fix

--- src/index.ts
+++ src/index.ts
@@ -1,86 +1,55 @@
 import fs from "fs"
 import id3 from "node-id3"
-import mri from "mri"
-
-const argv = process.argv.slice(2)
-const options = mri(argv, {
-  boolean: "noFeat",
-  default: {
-    noFeat: false,
-  },
-})
-
-const noFeat = options.noFeat
 
 const files = fs.readdirSync("unprocessed")
 
 const artistRegex = new RegExp(/(.+?)[,&x] (.+)/)
-const featRegex = new RegExp(/(.+)( feat | feat\. )(.+)/)
+const featRegex = new RegExp(/(.+)( feat | feat\. )(.+)/i)
 
 for (const file of files) {
   const tags = id3.read("unprocessed/" + file)
 
   if (!tags.artist) {
-    console.log(`${file} has no artist.`)
+    console.log(`"${file}" has no artist.`)
     continue
   }
 
-  const match = tags.artist.match(artistRegex)
+  const featMatch = tags.artist.match(featRegex)
 
-  if (!match) {
-    const featMatch = tags.artist.match(featRegex)
+  if (!featMatch) {
+    const match = tags.artist.match(artistRegex)
 
-    if (!featMatch || noFeat) {
+    if (!match) {
       console.log(`Skipping "${file}". It only has one artist.`)
       continue
     }
 
-    const artists = processArtists(featMatch)
+    const artists = processArtists(`${match[1]}, ${match[2]}`)
     console.log(artists)
 
     continue
   }
 
-  const artists = processArtists(match)
+  const artists = processArtists(`${featMatch[1]}, ${featMatch[3]}`)
   console.log(artists)
 }
 
-function processArtists(match: RegExpMatchArray) {
-  let artists = match[1].trim().concat("", "; ")
-  let remainingArtists = match[2]
-
-  if (remainingArtists == " feat " || remainingArtists == " feat. ") {
-    remainingArtists = match[3]
-  }
+function processArtists(remainingArtists: string) {
+  let artists = ""
 
   while (true) {
     let artist = remainingArtists.match(artistRegex)
 
-    if (!artist || remainingArtists.match(featRegex)) {
-
-      artist = remainingArtists.match(featRegex)
+    if (!artist) {
+      artist = remainingArtists.match(/.+/)
 
       if (!artist) {
-
-        if (noFeat) {
-          break
-        }
-
-        artist = remainingArtists.match(/.+/)
-
-        if (!artist) {
-          break
-        }
-
-        artists = artists.concat("", `${artist[0].trim()};`)
-
         break
       }
 
-      artists = artists.concat("", `${artist[1].trim()}; `)
-      remainingArtists = artist[3]
+      artists = artists.concat("", `${artist[0].trim()};`)
 
-      continue
+      break
     }
 
     artists = artists.concat("", `${artist[1].trim()}; `)
