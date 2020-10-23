8c7a8e779f0e8847c8309cfe2bdeb57ef5ed69f4
Adam Langbert <adamhl@pm.me>
2020-10-23 09:28:40 +0200

    first release

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,76 @@
+import mri from "mri"
 import fs from "fs"
-import id3 from "node-id3"
+import readline from "readline"
+import id3, { update } from "node-id3"
 
-const files = fs.readdirSync("unprocessed")
+const argv = process.argv.slice(2)
+const options = mri(argv)
+const updateFiles = options.u
+
+const unprocessedPath = "unprocessed/"
+const processedPath = "processed/"
 
 const artistRegex = new RegExp(/(.+?)[,&x] (.+)/)
 const featRegex = new RegExp(/(.+)( feat | feat\. )(.+)/i)
 
-for (const file of files) {
-  const tags = id3.read("unprocessed/" + file)
+fs.writeFileSync("output.txt", "")
 
-  if (!tags.artist) {
-    console.log(`"${file}" has no artist.`)
-    continue
-  }
+async function run() {
+  const ignoreList = await getIgnoreList()
 
-  const featMatch = tags.artist.match(featRegex)
+  const files = fs.readdirSync(unprocessedPath)
 
-  if (!featMatch) {
-    const match = tags.artist.match(artistRegex)
+  for (const file of files) {
+    const tags = id3.read(unprocessedPath + file)
 
-    if (!match) {
-      console.log(`Skipping "${file}". It only has one artist.`)
+    if (!tags.artist) {
+      console.log(`"${file}" has no artist.`)
       continue
     }
 
-    const artists = processArtists(`${match[1]}, ${match[2]}`)
-    console.log(artists)
+    const featMatch = tags.artist.match(featRegex)
 
-    continue
+    if (!featMatch) {
+      const match = tags.artist.match(artistRegex)
+
+      if (!match) {
+        console.log(`Skipping "${file}". It only has one artist.`)
+        continue
+      }
+
+      const remainingArtists = `${match[1]}, ${match[2]}`
+      updateTags(file, remainingArtists, ignoreList)
+
+      continue
+    }
+
+    const remainingArtists = `${featMatch[1]}, ${featMatch[3]}`
+    updateTags(file, remainingArtists, ignoreList)
+  }
+}
+
+function updateTags(file: string, remainingArtists: string, ignoreList: string[]) {
+  if (shouldIgnore(remainingArtists, ignoreList)) {
+    console.log(`Skipping "${file}". It contains an artist in your ignore list.`)
+    return
   }
 
-  const artists = processArtists(`${featMatch[1]}, ${featMatch[3]}`)
-  console.log(artists)
+  const inputPath = unprocessedPath + file
+  const outputPath = processedPath + file
+
+  const before = id3.read(inputPath).artist
+  const artists = processArtists(remainingArtists)
+  const tags = { artist: artists }
+
+  fs.appendFileSync("output.txt", `${before} -> ${artists}\n\n`)
+
+  if (updateFiles) {
+    id3.update(tags, inputPath)
+
+    fs.rename(inputPath, outputPath, (err) => {
+      if (err) throw err
+    })
+  }
 }
 
 function processArtists(remainingArtists: string) {
@@ -58,3 +97,34 @@ function processArtists(remainingArtists: string) {
 
   return artists
 }
+
+async function getIgnoreList(): Promise<string[]> {
+  const ignore: string[] = []
+  const fileStream = fs.createReadStream("ignore.txt")
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+  })
+
+  for await (const line of rl) {
+    ignore.push(line)
+  }
+
+  return ignore
+}
+
+function shouldIgnore(remainingArtists: string, ignoreList: string[]) {
+  let shouldIgnore = false
+
+  for (const ignore of ignoreList) {
+    if (remainingArtists.includes(ignore)) {
+      shouldIgnore = true
+      break
+    }
+  }
+
+  return shouldIgnore
+}
+
+run()
